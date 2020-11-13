147fdcb09303497017deae02e4a83868924d42ad
Shane O'Moore <shaneiadt@gmail.com>
2020-11-13 17:20:20 +0000

    implement the joke telling and the laughs lol :D

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,35 @@
-console.log('let the joke telling begin');
+// API KEY VISIBLE = I DON'T REALLY CARE!
+const VOICE_RSS_API_KEY = "537cee61bb24474887b796bf0dd983da";
+
+const audio: HTMLAudioElement | null = document.querySelector("#audio");
+const button: HTMLButtonElement | null = document.querySelector("#button");
+
+function createAudioSrc(txt: string): string {
+    return `http://api.voicerss.org/?key=${VOICE_RSS_API_KEY}&hl=en-us&src=${txt}`;
+}
+
+async function getJoke() {
+    try {
+        const response = await fetch("https://icanhazdadjoke.com/", {
+            headers: {
+                'Accept': 'application/json'
+            }
+        });
+        const { joke } = await response.json();
+
+        return joke as string;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+button?.addEventListener("click", async () => {
+    const joke = await getJoke();
+
+    if (audio && joke) {
+        audio.src = createAudioSrc(joke);
+        audio.play();
+    }
+});
 
 export { };
